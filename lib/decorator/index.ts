e0ddc9ed82f5a821e64d70a2d53ae6f1bf81073a
lizhen <lizhen@ideal-link.cn>
2022-07-28 01:11:04 +0800

    chore: format

--- lib/decorator/index.ts
+++ lib/decorator/index.ts
@@ -9,7 +9,7 @@ type InitFunction = (app: Application) => void;
 const initFunctions: InitFunction[] = [];
 
 export const initApp = (app: Application) => {
-  initFunctions.forEach(fn => fn(app));
+  initFunctions.forEach((fn) => fn(app));
 };
 
 /**
@@ -36,43 +36,43 @@ export type RequestMappingOptions = {
  */
 export const RequestMapping =
   (options: string | string[] | RequestMappingOptions): MethodDecorator =>
-    (target, name, descriptor: TypedPropertyDescriptor<any>) => {
-      initFunctions.push(app => {
-        let opts: RequestMappingOptions;
-        if (typeof options === 'string' || Array.isArray(options)) {
-          opts = {
-            value: options,
-          };
-        } else {
-          opts = options;
-        }
+  (target, name, descriptor: TypedPropertyDescriptor<any>) => {
+    initFunctions.push((app) => {
+      let opts: RequestMappingOptions;
+      if (typeof options === 'string' || Array.isArray(options)) {
+        opts = {
+          value: options,
+        };
+      } else {
+        opts = options;
+      }
 
-        /* @ts-ignore */
-        const path = `${target.pathName}.${name}`;
-        app.logger.debug('RequestMapping init:', options, path);
-        const values: string[] = typeof opts.value === 'string' ? [ opts.value ] : opts.value;
-        values.forEach(value => {
-          if (Array.isArray(opts.method)) {
-            opts.method.forEach(item => {
-              app.router[item](value, _get(app, path));
-            });
-          } else if (opts.method != null) {
-            app.router[opts.method](value, _get(app, path));
-          } else {
+      /* @ts-ignore */
+      const path = `${target.pathName}.${name}`;
+      app.logger.debug('RequestMapping init:', options, path);
+      const values: string[] = typeof opts.value === 'string' ? [opts.value] : opts.value;
+      values.forEach((value) => {
+        if (Array.isArray(opts.method)) {
+          opts.method.forEach((item) => {
+            app.router[item](value, _get(app, path));
+          });
+        } else if (opts.method != null) {
+          app.router[opts.method](value, _get(app, path));
+        } else {
           // 默认走get
-            app.router.get(value, _get(app, path));
-          }
-        });
+          app.router.get(value, _get(app, path));
+        }
       });
-      return descriptor;
-    };
+    });
+    return descriptor;
+  };
 
 /**
  * ResponseBody修饰器, 功能类似 spring-boot中的 @ResponseBody
  */
 export const ResponseBody: MethodDecorator = (target, name, descriptor: TypedPropertyDescriptor<any>) => {
   const oriValue = descriptor.value;
-  descriptor.value = async function(...args) {
+  descriptor.value = async function (...args) {
     const { ctx } = this as { ctx: Context };
     ctx.body = await oriValue.apply(this, args);
   };
